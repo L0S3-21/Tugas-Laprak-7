3509806144b5b3c366777473d692df1066175718
L0S3-21 <julius.samuel1234567@gmail.com>
2025-10-28 21:26:00 +0700

    Add files via upload

--- Nomor1.java
+++ Nomor1.java
@@ -2,7 +2,7 @@ public class Nomor1 { // untuk membuat class
     public static void main(String[] args) { // untuk memulai program
         int hargaPerLiter = 6500, liter = 1; // untuk mendeklarasikan tipe dan menginisialisasi nilai variabel
 
-        System.out.println("Jumlah liter\t" + "Harga (Rp)");
+        System.out.println("Jumlah liter\t" + "Harga (Rp)"); // \t untuk memberikan perintah tab
         System.out.println("==============================");
         
         while (liter <= 20){ // untuk membuat kondisi looping sampai batas looping tercapai 
